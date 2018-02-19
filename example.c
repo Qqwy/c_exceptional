338000798f63f9fabde1317c82459fd182480e9b
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-02-19 16:22:55 +0100

    Print uncaught exception message when throwing outside of try-block

--- example.c
+++ example.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -8,6 +9,8 @@
 #define TP(x, y) TP2(x, y)
 
 static jmp_buf exception_env;
+static int inside_try_block = 0;
+
 
 #define try while(1)                                                    \
     if(0){                                                              \
@@ -17,6 +20,7 @@ static jmp_buf exception_env;
       for(jmp_buf my_env;;)                                             \
         if(0) {                                                         \
         TP(__try_block_unwind, __LINE__):                               \
+          --inside_try_block;                                           \
           memcpy(exception_env, my_env, sizeof(jmp_buf));               \
           printf("Unwinding stack: %p\n", &my_env);                     \
           goto TP(__try_block_finished, __LINE__);                      \
@@ -29,6 +33,7 @@ static jmp_buf exception_env;
                 else if(1){                                             \
                   printf("  Winding stack: %p\n", &my_env);             \
                   memcpy(my_env, exception_env, sizeof(jmp_buf));       \
+                  ++inside_try_block;                                   \
                   goto TP(__try_body_second_half, __LINE__);            \
                 } else                                                  \
                 TP(__try_body_second_half, __LINE__):                   \
@@ -37,13 +42,19 @@ static jmp_buf exception_env;
 
 
 #define catch(exception) else                                           \
-    for(int exception = _exception;exception_dispatcher < 2; ++exception_dispatcher) \
+    for(int exception = _exception; exception_dispatcher < 2; ++exception_dispatcher) \
       if(_exception && exception_dispatcher == 0 && memcpy(exception_env, my_env, sizeof(jmp_buf))) \
 
 
 #define finally else if(++exception_dispatcher)
 
-#define throw(exception) longjmp(exception_env, exception);
+#define throw(exception) do {                         \
+    if(!inside_try_block){                            \
+      printf("Uncaught exception: %d\n", exception);  \
+      exit(EXIT_FAILURE);                             \
+    }                                                 \
+    longjmp(exception_env, exception);                \
+  } while (0)                                         \
 
 int main(){
   int foo = 0;
@@ -55,15 +66,15 @@ int main(){
       throw(44);
     } catch (err_inner) {
       printf("Catching inner error %d\n", err_inner);
-      throw(43);
     } finally {
       printf("Inner finally\n");
     }
+    throw(43);
   } catch(err) {
     printf("foo catch: %d, err: %d\n", foo, err);
   } finally {
     printf("Foo finally %d\n", foo);
-  }
+    }
 
   return 0;
 }
